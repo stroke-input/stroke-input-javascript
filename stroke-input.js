f0225d20caa23a9fb26a8359987e0df0d170d6e0
Conway <yawnoc@pm.me>
2024-10-19 16:45:45 +0800

    Add working implementation of Trie with prefix lookup

--- stroke-input.js
+++ stroke-input.js
@@ -1,3 +1,75 @@
+class StrokeTrieNode
+{
+  childFromStroke = new Map();
+  isTerminal = false;
+  characters = "";
+
+  prefixCharacters()
+  {
+    let characters = this.characters;
+
+    for (const [_, child] of this.childFromStroke)
+    {
+      characters += child.prefixCharacters();
+    }
+
+    return characters;
+  }
+}
+
+class StrokeTrie
+{
+  rootNode = new StrokeTrieNode();
+
+  insert(strokeSequence, characters)
+  {
+    let node = this.rootNode;
+
+    for (const stroke of strokeSequence)
+    {
+      if (!node.childFromStroke.has(stroke))
+      {
+        node.childFromStroke.set(stroke, new StrokeTrieNode());
+      }
+      node = node.childFromStroke.get(stroke);
+    }
+    node.isTerminal = true;
+    node.characters = characters;
+  }
+
+  lookup(strokeSequence, lookupType)
+  {
+    let node = this.rootNode;
+
+    for (const stroke of strokeSequence)
+    {
+      if (!node.childFromStroke.has(stroke))
+      {
+        return ""
+      }
+      node = node.childFromStroke.get(stroke);
+    }
+
+    switch (lookupType)
+    {
+      case "exact":
+        return node.characters;
+
+      case "prefix":
+        return node.prefixCharacters();
+
+      default:
+        throw new Error(`bad lookupType ${lookupType}`);
+    }
+  }
+}
+
+class StrokeInputService
+{
+  #charactersFromStrokeDigitSequence = new StrokeTrie();
+}
+
+
 function isModified(event)
 {
   return event.ctrlKey || event.shiftKey || event.altKey || event.metaKey;
@@ -169,3 +241,16 @@ function keyListener(event)
 }
 
 document.addEventListener("keydown", keyListener);
+
+let st = new StrokeTrie();
+st.insert("1", "一");
+st.insert("11", "二");
+st.insert("111", "三");
+st.insert("1234", "木朩");
+st.insert("251", "卂口囗");
+st.insert("2511", "丮冃冄日曰");
+
+for (const s of ["1", "11", "111", "25", "251", "2511", "25112", "1234", "12345"])
+{
+  console.log([s, st.lookup(s, "exact"), st.lookup(s, "prefix")]);
+}
