5ee02001041798e4448616b80db5f70b645ce977
Conway <yawnoc@pm.me>
2024-10-20 14:16:48 +0800

    Add HTML meta description element

--- index.cmd
+++ index.cmd
@@ -2,6 +2,8 @@ OrdinaryDictionaryReplacement: #.boilerplate-properties-override
 - queue_position: BEFORE #boilerplate-properties
 - apply_mode: SEQUENTIAL
 * %title --> Stroke input method (筆畫輸入法): JavaScript
+* %head-elements-before-viewport -->
+    <meta name="description" content="A JavaScript implementation of the stroke input method (筆畫輸入法), for desktop browser.">
 * %head-elements-after-viewport -->
     <script defer src="stroke-input.js"></script>
 
